3d574383f56271602abacedebc78539d4ebc0fe1
dvisanand <51434625+dvisanand@users.noreply.github.com>
2019-06-24 10:46:21 +0530

    Update pkr-prov.sh

--- conf/pkr-prov.sh
+++ conf/pkr-prov.sh
@@ -2,6 +2,7 @@
 yum update -y
 yum install -y git
 yum install -y nginx
+copy ./conf/nginx.conf /etc/nginx/nginx.conf
 curl -sL https://rpm.nodesource.com/setup_8.x | bash -
 yum install -y nodejs
 export NPM_CONFIG_PREFIX=~/.npm-global
